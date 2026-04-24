d1a78b294c045518079a840f136dca04e55cc669
edcasill <acasillasval@gmail.com>
2026-04-23 23:04:29 -0600

    sentado y parado funcional

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ USUARIOS = ["Camara_1", "Camara_2"]
 IPS = ["192.168.0.158", "192.168.0.87"]
 CONTRASENIA = "Cinvestav.101"
 frame_count = 5  # procesa cada 5 frames
+UMBRAL_SENTADO = 0.4
--- identification.py
+++ identification.py
@@ -19,10 +19,10 @@ def enviar_a_api(estado):
 
 
 def video_model():
-    skeleton = YOLO('yolov8n-pose.pt')  # pretrained YOLO model with pose detection
-    model = YOLO('yolov8n.pt')
+    model = YOLO('yolov8n-pose.pt')  # pretrained YOLO model with pose detection
+    # model = YOLO('yolov8n.pt')
     url = f"rtsp://{constants.USUARIOS[1]}:{constants.CONTRASENIA}@{constants.IPS[1]}/stream1"
-    frame_count = 5  # procesa cada 5 frames
+    frame_count = 0  # procesa cada 5 frames
 
     # API data
     ultimo_estado = None
@@ -45,35 +45,88 @@ def video_model():
         frame_count += 1
         if frame_count % 5 == 0: # Saltamos cuadros para mantener el tiempo real
             # Ejecutar detección (clase 0 es 'person' en COCO dataset)
-            results = model(frame, classes=[0], verbose=False, imgsz=320)
+            # results = model(frame, classes=[0], verbose=False, imgsz=320)
+            results_skeleton = model(frame, verbose=False, imgsz=320)
             last_box = []  # limpia los boxes
             
-            for r in results:
-                for box in r.boxes:
-                    # Obtener coordenadas: x1, y1 (arriba izq), x2, y2 (abajo der)
-                    x1, y1, x2, y2 = box.xyxy[0].tolist()
-                    w = x2 - x1
-                    h = y2 - y1
-                    
-                    # Lógica simple: Si la altura es menor a 1.2 veces el ancho, está sentado/acostado
-                    # Puedes ajustar este umbral según la posición de tu cámara
-                    relacion_aspecto = h / w
-                    estado_actual = "Sentado/Inactivo" if relacion_aspecto < 1.3 else "De pie/Activo"
-                    last_box.append((int(x1), int(y1), int(x2), int(y2), estado_actual))
+            # for r in results:
+            for r in results_skeleton:
+                if r.keypoints is None or r.boxes is None:
+                    continue
 
-                    # Control de la API
-                    tiempo_actual = time.time()
-                    # Se envía SÓLO si el estado cambió, o si ya pasó el tiempo de cooldown
-                    if estado_actual != ultimo_estado or (tiempo_actual - ultimo_envio > COOLDOWN_API):
-                        enviar_a_api(estado_actual)
-                        ultimo_estado = estado_actual
-                        ultimo_envio = tiempo_actual
+                for i, box in enumerate(r.boxes):
+                    if int(box.cls[0]) == 0:
 
-        for x1, y1, x2, y2, estado in last_box:
-            # Dibujar en pantalla
-            color = (0, 255, 0) if "Activo" in estado_actual else (0, 0, 255)
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-            cv2.putText(frame, estado, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                        # Obtener coordenadas: x1, y1 (arriba izq), x2, y2 (abajo der)
+                        x1, y1, x2, y2 = box.xyxy[0].tolist()
+                        # w = x2 - x1
+                        # h = y2 - y1
+                        kpts = r.keypoints.data[i]  # verifica que hay un esqueleto
+                        # hay tab
+                        # Lógica simple: Si la altura es menor a 1.2 veces el ancho, está sentado/acostado
+                        # Puedes ajustar este umbral según la posición de tu cámara
+                        """relacion_aspecto = h / w
+                        estado_actual = "Sentado/Inactivo" if relacion_aspecto < 1.3 else "De pie/Activo"
+                        last_box.append((int(x1), int(y1), int(x2), int(y2), estado_actual))
+
+                        # Control de la API
+                        tiempo_actual = time.time()
+                        # Se envía SÓLO si el estado cambió, o si ya pasó el tiempo de cooldown
+                        if estado_actual != ultimo_estado or (tiempo_actual - ultimo_envio > COOLDOWN_API):
+                            enviar_a_api(estado_actual)
+                            ultimo_estado = estado_actual
+                            ultimo_envio = tiempo_actual
+
+            for x1, y1, x2, y2, estado in last_box:
+                # Dibujar en pantalla
+                color = (0, 255, 0) if "Activo" in estado_actual else (0, 0, 255)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+                cv2.putText(frame, estado, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)"""
+                        try:
+                            y_cadera = kpts[11][1]
+                            y_rodilla = kpts[13][1]
+                            y_hombro = kpts[5][1]
+                            
+                            # Filtro de confianza: Si no ve bien las piernas, no adivina
+                            confianza_rodilla = kpts[13][2]
+                            
+                            if confianza_rodilla > 0.4:
+                                # Aplicamos tu lógica geométrica
+                                dif_pierna = abs(y_cadera - y_rodilla)
+                                dif_torso = abs(y_hombro - y_cadera)  # tomamos en cuenta la posicion del torso
+                                
+                                if dif_torso < 40:
+                                    estado = "Acostado"
+                                    color = (255, 0, 0)
+
+                                # Si la distancia en Y es muy pequeña, los muslos están horizontales
+                                if dif_pierna < 55: # Ajustar este pixelaje a tu cámara
+                                    estado = "Sentado"
+                                    color = (0, 0, 255)
+                                else:
+                                    estado = "De pie"
+                                    color = (0, 255, 0)
+                            else:
+                                estado = "Piernas Ocultas"
+                                color = (255, 165, 0) # Naranja
+                                
+                        except IndexError:
+                            estado = "Analizando..."
+                            color = (255, 255, 255)
+
+                        last_box.append((int(x1), int(y1), int(x2), int(y2), estado, color))
+
+                        # Lógica de la API 
+                        tiempo_actual = time.time()
+                        if estado != ultimo_estado or (tiempo_actual - ultimo_envio > COOLDOWN_API):
+                            enviar_a_api(estado)
+                            ultimo_estado = estado
+                            ultimo_envio = tiempo_actual
+
+                        # Dibujamos el resultado del Double Check
+        for x1, y1, x2, y2, estado, color in last_box:
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame, estado, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
         cv2.imshow("Deteccion de actividad", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
